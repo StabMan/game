cfcb84b6c0cdf138de3108b1ebdcf9b7a0070a73
Konstantin <63863905+StabMan@users.noreply.github.com>
2024-03-05 16:06:41 +0700

    Add files via upload

---
+++ BossHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+
+    public int health = 200;
+    public Animator animator;
+
+    public GameObject deathEffect;
+
+    public bool isInvulnerable = false;
+
+    public void TakeDamage(int damage)
+    {
+        if (isInvulnerable)
+            return;
+
+        health -= damage;
+
+
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        animator.SetTrigger("Dead");
+        Destroy(gameObject);
+    }
+
+}
